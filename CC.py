04c9fa62f31bef5b3f8478396654420703ef700e
Nick Royce <96420446+NickRoyce@users.noreply.github.com>
2025-04-30 18:52:09 +0700

    CC.py

--- CC.py
+++ CC.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
-
+print("Welcome to Caesar Cypher text encryptor and decryptor!")
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
